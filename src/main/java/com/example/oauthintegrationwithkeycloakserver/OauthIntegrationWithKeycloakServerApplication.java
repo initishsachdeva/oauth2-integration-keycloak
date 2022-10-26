9b24100050ec2c200c0937df57654ee585812c7c
NitishSachdeva <nitishsachdeva39@gmail.com>
2022-10-26 22:58:41 +0530

    proj structure

---
+++ src/main/java/com/example/oauthintegrationwithkeycloakserver/OauthIntegrationWithKeycloakServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauthintegrationwithkeycloakserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthIntegrationWithKeycloakServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthIntegrationWithKeycloakServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oauthintegrationwithkeycloakserver/OauthIntegrationWithKeycloakServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauthintegrationwithkeycloakserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthIntegrationWithKeycloakServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
